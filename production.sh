8d6915468c9d219194babb06890f1ec290ee7dfa
Niall Wallace <nj.wallace@outlook.com>
2017-04-05 14:07:19 +0100

    stopping pm2 services before production provision

--- production.sh
+++ production.sh
@@ -1,12 +1,13 @@
 
 # remove the old file and add our one
 sudo rm /etc/nginx/sites-available/default
-sudo cp nginx.default /etc/nginx/sites-available/default
+sudo cp /home/ubuntu/env/nginx.default /etc/nginx/sites-available/default
 
 # finally, restart the nginx service so the new config takes hold
-sudo service nginx restart
+sudo service nginx restart 
 
 # install the node webhook handler and start the hook server
 cd /home/ubuntu/app
 npm install 
+pm2 stop all 
 pm2 start app.js
